5e9cc9bc918cb131cc37d87d4a7fe8bb32db4ca3
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-10-19 08:53:51 -0700

    added request-lines, fixed backend bug

--- PRS/PRS/Data/PRSContext.cs
+++ PRS/PRS/Data/PRSContext.cs
@@ -9,15 +9,22 @@ namespace PRS.Data
 {
     public class PRSContext : DbContext
     {
-        public PRSContext (DbContextOptions<PRSContext> options)
-            : base(options)
-        {
-        }
 
         public DbSet<User> Users { get; set; } = default!;
         public DbSet<Vendor> Vendors { get; set; } = default!;
         public DbSet<Product> Products { get; set; } = default!;
         public DbSet<Request> Requests { get; set; } = default!;
         public DbSet<RequestLine> RequestLines { get; set; } = default!;
+        
+        public PRSContext (DbContextOptions<PRSContext> options) : base(options) { }
+        public PRSContext() { }
+
+        // FOR AUTO TESTING WITH XUNIT
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseSqlServer("server=localhost\\sqlexpress;database=PrsDb;trusted_connection=true;trustServerCertificate=true;");
+            }
+        }
+
     }
 }
